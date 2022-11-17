01032bd9083ae9544e4f848226ac6deb1c1915eb
Abhinandan Shrestha <shtabhi@gmail.com>
2022-11-17 19:12:17 +0545

    testBranch

---
+++ src/components/Datafetch.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import React,{useState,useEffect} from 'react'
+
+function Datafetch() {
+
+  return (
+
+  )
+}
+
+export default Datafetch
--- src/components/Traffic.js
+++ src/components/Traffic.js
@@ -1,10 +1,34 @@
-import React from 'react';
-
+import React,{useState,useEffect}  from 'react';
+import axios from 'axios'
 export default function Traffic() {
-
+    const [json,setJson]=useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:5000/data')
+        .then(res=>{
+            // console.log(res.data)
+            setJson(res.data)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },[])
     return (
         <div className='traffic'>
-            Traffic
+            <table>
+                {/* <tr>
+                    {Object.keys(json[0]).map((key) => (
+                    <th>{key}</th>
+                    ))}
+                </tr> */}
+                    {json.map((obj) => (
+                        <tr>
+                          {Object.values(obj).map((val) => (
+                            <td>{val}</td>
+                          ))}
+                        </tr>
+                      ))}
+                        
+            </table>
         </div>
     );
 }
