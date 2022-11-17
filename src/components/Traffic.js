48c4c8535cea2b5c4d8350d7928f8021bbcc8fcd
Bibek Manandhar <manandhar.bibek01@gmail.com>
2022-11-17 12:20:30 +0545

    material-react-table

--- src/components/Traffic.js
+++ src/components/Traffic.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useMemo } from "react";
+import MaterialReactTable from "material-react-table";
 
 export default function Traffic() {
+    const [data, setData] = useState([]);
+
+    const fetchData = () => {
+        fetch("http://localhost:5000/data")
+            .then((res) => res.json())
+            .then((data) => JSON.parse(data))
+            .then((data) => {
+                console.log(data);
+                setData(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: "a.x",
+                header: "a->x",
+            },
+            {
+                accessorKey: "a.y",
+                header: "a->y",
+            },
+            {
+                accessorKey: "b",
+                header: "b",
+            },
+            {
+                accessorKey: "c",
+                header: "c",
+            },
+        ],
+        []
+    );
 
     return (
-        <div className='traffic'>
+        <div className="traffic">
             Traffic
+            {data && <MaterialReactTable columns={columns} data={data} />}
         </div>
     );
 }
