779b0439212210df41ac64b13344743388318abd
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-02-02 14:07:18 +0545

    Slice implemented for form

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import monitorReducer from "./monitorSlice";
 import trafficReducer from "./trafficSlice";
+import formReducer from "./formSlice";
 import logReducer from "./logSlice";
 import scatterReducer from "./scatterSlice";
 
@@ -8,6 +9,7 @@ export const store = configureStore({
     reducer: {
         monitor: monitorReducer,
         traffic: trafficReducer,
+        form: formReducer,
         log: logReducer,
         scatter: scatterReducer,
     },
